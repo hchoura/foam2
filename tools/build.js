7f74089191675141f982f20599d12c4a0666e907
hchoura <choura.hassene@gmail.com>
2017-10-24 16:56:54 +0100

    Can packages all models into a single .js file.

--- tools/build.js
+++ tools/build.js
@@ -19,6 +19,7 @@
 
 var flags = {};
 var otherLanguages = [ 'java', 'swift' ];
+var fs_ = require('fs');
 
 if ( process.argv.length > 2 ) {
   process.argv[2].split(',').forEach(function(f) {
@@ -33,9 +34,43 @@ if ( process.argv.length > 2 ) {
   }
 }
 
-var outfile = __dirname + '/../foam-bin.js';
+var outfile;
 if ( process.argv.length > 3 ) {
-  outfile = process.argv[3];
+  outfile = __dirname + process.argv[3];
+} else {
+  outfile = __dirname + '/../foam-bin.js';
+}
+
+var modelToBuild;
+if ( process.argv.length > 4 ) {
+  modelToBuild = __dirname + '/../src/' + process.argv[4].replace(/[.]/g, '/') + '.js';
+}
+
+if ( modelToBuild !== undefined ) {
+  require('../src/foam.js');
+  require('../src/foam/core/FObject.js');
+  require(modelToBuild);
+
+  var requiredFiles = new Set(['foam/core/poly', 'foam/core/lib', 'foam/core/stdlib',
+    'foam/core/events', 'foam/core/Context', 'foam/core/Boot', 'foam/core/FObject',
+    'foam/core/Model', 'foam/core/Property', 'foam/core/Method', 'foam/core/Boolean',
+    'foam/core/AxiomArray', 'foam/core/EndBoot'
+  ]);
+  //TODO to verify in case of multipule class in the same file.
+
+  collectExtendsFiles = function(fl) {
+    try {
+      cl = eval(fl);
+    } catch (e) {
+      console.log(e);
+    }
+    requiredFiles.add(fl);
+    for (let item of cl.getAxiomsByClass(foam.core.Requires)) {
+      requiredFiles.add(item.path);
+    }
+    return requiredFiles;
+  };
+  requiredFiles = collectExtendsFiles(process.argv[4]);
 }
 
 var payload = '';
@@ -46,19 +81,53 @@ var env = {
     }).map(function(f) {
       return f.name;
     }).forEach(function(f) {
-      var data = require('fs').readFileSync(__dirname + '/../src/' + f + '.js').toString();
+      var data = fs_.readFileSync(__dirname + '/../src/' + f + '.js').toString();
       payload += data;
     });
   }
 };
 
-var data = [ require('fs').readFileSync(__dirname + '/../src/files.js') ];
-if ( flags.nanos ) {
-  data.push(
-      require('fs').readFileSync(__dirname + '/../src/foam/nanos/nanos.js'));
+if ( process.argv.length > 2 && modelToBuild === undefined ) {
+  var data = [fs_.readFileSync(__dirname + '/../src/files.js')];
+  if ( flags.nanos ) {
+    data.push(
+      fs_.readFileSync(__dirname + '/../src/foam/nanos/nanos.js'));
+  }
+  for (var i = 0; i < data.length; i++) {
+    with(env) {
+      eval(data[i].toString());
+    }
+  }
+} else {
+  for (let item of requiredFiles) {
+    if ( item !== undefined ) {
+      console.log(item);
+      requiredFile = __dirname + '/../src/' + item.replace(/[.]/g, '/') + '.js';
+      try {
+        payload += fs_.readFileSync(requiredFile).toString();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 }
 
-for ( var i = 0; i < data.length; i++ ) {
-  with (env) { eval(data[i].toString()); }
-}
-require('fs').writeFileSync(outfile, payload);
+fs_.writeFileSync(outfile, payload);
+
+var UglifyJS = require('uglify-es');
+
+var code = outfile;
+var options = {
+  ecma: 6, // specify one of: 5, 6, 7 or 8
+  nameCache: null, // or specify a name cache object
+  toplevel: false,
+  ie8: false,
+  warnings: false,
+  mangle: {},
+  compress: {},
+  output: {},
+};
+
+fs_.writeFileSync('output.min.js', UglifyJS.minify({
+  outfile: fs_.readFileSync(outfile, 'utf8')
+}, options).code, 'utf8');
